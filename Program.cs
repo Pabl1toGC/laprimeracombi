2f0536fcda1d1c7b675ad43b5685d2833e5baf78
Pabl1toGC <gcp0033703@est.univalle.edu>
2023-05-10 16:10:53 -0400

    la segunda commit

--- Program.cs
+++ Program.cs
@@ -6,8 +6,16 @@
         {
             Console.WriteLine("RURU");
             int n1,n2;
+            string nom;
             n1 = n2 = 1;
             Console.WriteLine(n1 + n2);
+            nom = leerdato();
+        }
+        public static string leerdato()
+        {
+            string nom;
+            nom= Console.ReadLine();
+            return nom;
         }
     }
 }
