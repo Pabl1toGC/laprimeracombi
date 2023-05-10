b9f11ca7cfa274016ce4fab63e30f768f3816c01
Pabl1toGC <gcp0033703@est.univalle.edu>
2023-05-10 16:04:38 -0400

    el primer commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace git_D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RURU");
+            int n1,n2;
+            n1 = n2 = 1;
+            Console.WriteLine(n1 + n2);
+        }
+    }
+}
